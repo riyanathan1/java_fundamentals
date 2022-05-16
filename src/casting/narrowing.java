f836ec7546f5dcb4c9e268c64f1d1be6f62db6a6
riyanathan <riyaunathan@gmail.com>
2022-05-16 19:18:22 +0100

    fundamentals labs

---
+++ src/casting/narrowing.java
@@ -0,0 +1,24 @@
+package casting;
+
+public class narrowing
+{
+
+
+    public static void main(String[] args)
+    {
+        double x = 40;
+
+        int z = (int) x;
+
+        System.out.println("int value " + z );
+
+        double d = 300.89;
+
+        long l = (long)d;
+        System.out.println("Long value " + l);
+
+        int i = (int)l;
+
+        System.out.println("int value " + i);
+    }
+}
