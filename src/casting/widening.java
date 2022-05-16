66df5888ca06de68c90c5c8036af5f8623a9d7c5
riyanathan <riyaunathan@gmail.com>
2022-05-16 19:16:21 +0100

    fundamentals labs

---
+++ src/casting/widening.java
@@ -0,0 +1,15 @@
+package casting;
+
+public class widening {
+
+    public static void main(String[] args)
+    {
+        int i = 200;
+
+        long l = i;
+        float f = l;
+        System.out.println("Int value "+i);
+        System.out.println("Long value "+l);
+        System.out.println("Float value "+f);
+    }
+}
