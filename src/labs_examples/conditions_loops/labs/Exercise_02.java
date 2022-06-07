136148934daf1a22046bbbaf444bc1901c9f4cb8
riyanathan <riyaunathan@gmail.com>
2022-06-07 17:29:16 +0100

    fundamentals labs

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 2: Days of the week
  *
@@ -19,6 +21,24 @@ public class Exercise_02 {
         // 2) prompt user
         // 3) assign input to variable as int
         // 4) write completed code here
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter a number between 1-85 ");
+        int day = scanner.nextInt();
+        if(day == 1 )
+            System.out.println( " monday");
+        else if(day == 2 )
+            System.out.println( "tuesday");
+        else if(day == 3 )
+            System.out.println( "wednesday");
+        else if(day == 4 )
+            System.out.println( "thursday");
+        else if(day == 5 )
+            System.out.println( "friday");
+        else if(day == 6 )
+            System.out.println( "saturday");
+        else if(day == 7 )
+            System.out.println( "sunday");
+        else if(day == 8 )
+            System.out.println("other");
     }
 }
