464ebf35baac56bb3f3566e9aebf22c9e2eddb6b
riyanathan <riyaunathan@gmail.com>
2022-06-09 14:02:00 +0100

    fundamentals labs

--- src/labs_examples/conditions_loops/labs/Exercise_10.java
+++ src/labs_examples/conditions_loops/labs/Exercise_10.java
@@ -8,4 +8,14 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_10 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; ++i) {
+
+            if (i > 4 && i < 9) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
 }
